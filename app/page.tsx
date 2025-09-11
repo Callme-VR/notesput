c7cbef4e73eeb5407e1174da969bb4f7fc46ccc0
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-09-11 16:17:44 +0530

    add hero section and header components and features  section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Features from "@/components/features";
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 
 export default function Home(){
   return(
-    <div className="text-3xl">
-      vishal
-    </div>
+    <main>
+      <HeroHeader />
+      <HeroSection />
+      <Features />
+    </main>
   )
 }
